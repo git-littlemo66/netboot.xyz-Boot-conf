9c62484541768c38b58637d75b1e58cec92eb063
LIttleMo <56909488+moqu66@users.noreply.github.com>
2021-12-12 16:48:02 +0800

    Update bootConf.sh

--- bootConf.sh
+++ bootConf.sh
@@ -54,7 +54,7 @@ EOF
 
 cat > /etc/grub.d/40_custom << EOF
 #!/bin/sh
-exec tail -n +3 $0
+exec tail -n +3 \$0
 # This file provides an easy way to add custom menu entries.  Simply type the
 # menu entries you want to add after this comment.  Be careful not to change
 # the 'exec tail' line above.
